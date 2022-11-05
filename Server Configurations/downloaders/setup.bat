06ad5972935b1bd7905be5caa18593a8fa9e5aa4
Daniel <me@daaan.uk>
2022-11-05 19:51:27 +0000

    syntax fixes

--- Server Configurations/downloaders/setup.bat
+++ Server Configurations/downloaders/setup.bat
@@ -1,11 +1,9 @@
-ECHO "This file will download the latest version of Purpur, create an eula text file with the value of True, create a run.bat file with the required startup args using 4gb ram. This file will then close and delete."
+mkdir %USERPROFILE%\MinecraftServer
 
-mkdir %USERPROFILE%/Desktop/MinecraftServer
+echo "eula=true" > %USERPROFILE%\MinecraftServer\eula.txt
 
-powershell -Command "Invoke-WebRequest https://api.purpurmc.org/v2/purpur/latest/latest/download  -OutFile %USERPROFILE%/Desktop/MinecraftServer/server.jar"
+echo "java -Xmx4G -Xms4G -jar server.jar nogui" > %USERPROFILE%\MinecraftServer\run.bat
 
-TYPE eula=true > %USERPROFILE%/Desktop/MinecraftServer/eula.txt
+powershell -Command "Invoke-WebRequest https://api.purpurmc.org/v2/purpur/latest/latest/download  -OutFile %USERPROFILE%\MinecraftServer\server.jar"
 
-TYPE "java -Xmx6G -Xms6G -jar server.jar nogui" > %USERPROFILE%/Desktop/MinecraftServer/run.bat
-
-del %0
+del setup.bat
